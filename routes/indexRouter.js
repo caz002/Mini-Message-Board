ce94a22b76aedee8491a6dbff48133e346513fd2
caz002 <caz002@ucsd.edu>
2025-06-02 17:35:30 -0700

    added basic styling

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,14 +4,14 @@ const indexRouter = Router();
 
 const messages = [
     {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
+      text: "Welcome to the Message Board! Feel free to make a new post and upload a comment to share your thoughts.",
+      user: "Amy",
+      added: new Date().toLocaleString().split(',')[0]
     },
     {
       text: "Hello World!",
       user: "Charles",
-      added: new Date()
+      added: new Date().toLocaleString().split(',')[0]
     }
   ];
 
@@ -21,7 +21,7 @@ const links = [
 indexRouter.get("/", (req, res) => res.render("index", { messages: messages, links: links }));
 indexRouter.get("/new", (req, res) => res.render("form"));
 indexRouter.post("/new", (req, res) => {
-    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date() });
+    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date().toLocaleString().split(',')[0] });
     res.redirect("/");
 })
 module.exports = indexRouter;
